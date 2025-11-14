e87a0c2f4e457f2c0412b0d10354f83bdbbffc2c
AN HOANG <anhoang5205@gmail.com>
2025-11-15 03:24:37 +0700

    first commit main.py

---
+++ hd.py
@@ -0,0 +1,9 @@
+#repository = project
+#cau hinh name va email: git config --global user.email (email)
+#tao git local repo client (thu muc goc du an): git init
+#=> tao repo tren may de link toi repo tren server
+#ket noi voi server: git remote add origin (duong dan project)
+    # kiem tra git remote -v => ra nguon goc du an
+#dua tai lieu len remote server (lam thuong xuyen)
+#git add (ten file, folder, tat ca (-A)): chon cac file dua len server
+#=> kiem tra: git status
